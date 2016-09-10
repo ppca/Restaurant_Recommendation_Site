22fe7dcbd114635ee1683cc3154b73bbeb82b629
ppca <xyzheng@uchicago.edu>
2016-09-10 10:40:34 -0700

    initial commit

---
+++ src/api/RecommendRestaurants.java
@@ -0,0 +1,65 @@
+package api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class RecommendRestaurants
+ */
+@WebServlet("/recommendation")
+public class RecommendRestaurants extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final DBConnection connection = new MySQLDBConnection();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RecommendRestaurants() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		JSONArray array = null;
+
+		if (request.getParameterMap().containsKey("user_id")) {
+			String userId = request.getParameter("user_id");
+			array = connection.recommendRestaurants(userId);
+
+		}
+
+		RpcParser.writeOutput(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
